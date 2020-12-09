7c8c234aad2709c20e3a42413a5d1bcc68eb036f
Skander <skander.kc@gmail.com>
2020-12-08 21:02:54 -0500

    Cleanup day 8

--- Advent-2020/Days/Day8.cs
+++ Advent-2020/Days/Day8.cs
@@ -9,97 +9,85 @@ namespace Advent_2020.Days
     {
         protected override void Run()
         {
-            int accumPart1 = 0, accumPart2 = 0;
-
             List<Instruction> instructions = InputLines.Select((inp) => new Instruction(inp)).ToList();
 
             // Part 1
-            RunInstructions(instructions, ref accumPart1);
+            int accumPart1 = RunInstructions(instructions).Item2;
             AnswerPart1 = accumPart1.ToString();
 
             // Part 2
             List<Instruction> fixedInstructions = GetFixedInfiniteLoop(instructions);
-            RunInstructions(fixedInstructions, ref accumPart2);
+            int accumPart2 = RunInstructions(fixedInstructions).Item2;
             AnswerPart2 = accumPart2.ToString();
         }
 
         
-        private List<Instruction> GetFixedInfiniteLoop(List<Instruction> initialInstructions)
+        private List<Instruction> GetFixedInfiniteLoop(List<Instruction> instructions)
         {
-            var allNopJmpInstructions = initialInstructions.Where(x => x.Action == "nop" || x.Action == "jmp").ToList();
+            var allNopJmpInstructions = instructions.Where(x => x.Action == "nop" || x.Action == "jmp").ToList();
+
             foreach (Instruction instruction in allNopJmpInstructions)
             {
-                // Create a new copy
-                var copyInstruction = (Instruction)instruction.Clone();
-                var instructionsToTest = initialInstructions.ConvertAll(x => new Instruction(x.Action, x.Value));
+                int instructionIndex = instructions.IndexOf(instruction);
 
                 // Toggle action between nop -> jmp and jmp -> nop
-                copyInstruction.Action = instruction.Action == "nop" ? "jmp" : "nop";
-                int instructionIndex = initialInstructions.IndexOf(initialInstructions.FirstOrDefault(x => x.Id == instruction.Id));
-                instructionsToTest[instructionIndex] = copyInstruction;
+                instruction.Action = instruction.Action == "nop" ? "jmp" : "nop";
+                instructions[instructionIndex] = instruction;
 
-                int sum = 0;
-                bool ranSuccesfully = RunInstructions(instructionsToTest, ref sum);
+                bool ranSuccesfully = RunInstructions(instructions).Item1;
+                instructions.ForEach(x => x.IsExecuted = false);
+
                 if (ranSuccesfully)
                 {
-                    instructionsToTest.ForEach(x => x.IsExecuted = false);
-                    return instructionsToTest;
+                    return instructions;
+                }
+                else
+                {
+                    instruction.Action = instruction.Action == "nop" ? "jmp" : "nop";
+                    instructions[instructionIndex] = instruction;
                 }
             }
 
             return null;
         }
 
-        private bool RunInstructions(List<Instruction> instructions, ref int sum)
+        private Tuple<bool, int> RunInstructions(List<Instruction> instructions)
         {
+            int accum = 0;
+
             for (int i = 0; i < instructions.Count; i++)
             {
                 Instruction instruction = instructions[i];
 
                 if (instruction.IsExecuted)
                 {
-                    return false;
+                    // Infinite loop
+                    return Tuple.Create(false, accum);
                 }
 
-                if (instruction.Action == "acc") sum += instruction.Value;
+                if (instruction.Action == "acc") accum += instruction.Value;
                 else if (instruction.Action == "jmp") i += instruction.Value - 1;
 
                 instruction.IsExecuted = true;
             }
 
             // Has run successfully without infinite loop
-            return true;
+            return Tuple.Create(true, accum);
         }
 
     }
 
-    class Instruction : ICloneable
+    class Instruction
     {
-        public Guid Id { get;  }
         public string Action { get; set; }
         public int Value { get; set; }
         public bool IsExecuted { get; set; }
-
         public Instruction(string input)
         {
-            Id = Guid.NewGuid();
-
             var g = new Regex(@"([a-z]{3}) ([+-])(\d+)").Match(input).Groups;
             Action = g[1].Value;
             int sign = g[2].Value == "-" ? -1 : 1;
             Value = int.Parse(g[3].Value) * sign;
-        }
-
-        public Instruction(string action, int value)
-        {
-            Id = Guid.NewGuid();
-            Action = action;
-            Value = value;
-        }
-
-        public object Clone()
-        {
-            return new Instruction(Action, Value);
         }
     }
 }
